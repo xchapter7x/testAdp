2d96e4ec287e789cad655fc9dc949a96b6333720
John Calabrese <xchapter7x@gmail.com>
2013-11-22 09:16:48 -0500

    swapping out some npms
    removing mongoose as well as nodeunit and nodecover
    in favor of jasmine and blanket

--- cookbooks/npm_package_installs/recipes/default.rb
+++ cookbooks/npm_package_installs/recipes/default.rb
@@ -5,8 +5,8 @@ bash 'install jshint to the VM using npm' do
         sudo npm install jshint -g
         sudo npm install imagemagick-native -g
         sudo npm install mongodb -g
-        sudo npm install nodecover -g
-        sudo npm install mongoose -g
+        sudo npm install jasmine-node -g
+        sudo npm install blanket -g
         sudo npm install express -g
         sudo gem install compass
         sudo npm install compass -g
