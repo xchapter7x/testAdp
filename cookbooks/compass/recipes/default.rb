f468f1f1de0e316881edf4ded5a7614bd0e7a369
John Calabrese <xchapter7x@gmail.com>
2013-12-02 15:37:32 -0500

    fixing cookbooks[2]
    this should just be compass specific things
    as we no long install npm's here but rather
    do it as part of the code repo

--- cookbooks/compass/recipes/default.rb
+++ cookbooks/compass/recipes/default.rb
@@ -3,6 +3,6 @@ bash 'install compass and related utils' do
     user node[:default_owner_user]
     code <<-EOH
         sudo gem install compass
-        sudp gem install bootstrap-sass
+        sudo gem install bootstrap-sass
     EOH
 end
